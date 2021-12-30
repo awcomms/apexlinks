fa17ca6a4ece16582527ae59facfc49602049bb2
EdwardGold Edem Hogan <edge37@outlook.com>
2021-12-30 11:05:54 +0100

    save

--- src/routes/auth/exit.js
+++ src/routes/auth/exit.js
@@ -8,7 +8,7 @@ export async function post(req) {
             'set-cookie': 'token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC'
         },
         body: {
-            ok: true
+            ok:true
         }
     }
 }
